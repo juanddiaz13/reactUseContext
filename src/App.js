cf7bf752a42a2364c7d91031ba4c8d17adc408c8
juanddiaz13 <juanddiaz13@gmail.com>
2022-04-07 22:28:19 -0500

    avance

--- src/App.js
+++ src/App.js
@@ -3,22 +3,49 @@ import { ContactForm } from './pages/ContactForm';
 import { NavBar } from './components/NavBar';
 import { Gallery } from './pages/Gallery';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import { useEffect, useState } from "react";
+
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
+  }
+};
+
+const ThemeContext = React.createContext({
+  theme: themes.light,
+  setTheme : () => {}
+});
 
 function App() {
+  const [theme, setTheme] = useState(themes.light);
+  const value = {theme, setTheme};
   return (
-    <>
-      <Router>
-        <NavBar></NavBar>
-        <Routes>
-          <Route exact path='/gallery' element={<Gallery></Gallery>} />
-          <Route
-            exact
-            path='/contact-form'
-            element={<ContactForm></ContactForm>}
-          />
-        </Routes>
-      </Router>
-    </>
+    <ThemeContext.Provider value={value}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const {theme, setTheme} = useContext(ThemeContext);
+  return (
+    <button onClick={() => setTheme(theme==themes.light?themes.dark:themes.light)} style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
   );
 }
 
